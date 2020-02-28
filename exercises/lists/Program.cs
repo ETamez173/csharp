afeb3c4a6dad4a1e7c3cf978ca2e0b47380b7a24
ETamez9173 <elitamez17@gmail.com>
2020-02-28 14:10:03 -0600

    finished all Planets

--- exercises/lists/Program.cs
+++ exercises/lists/Program.cs
@@ -72,15 +72,7 @@ namespace lists {
             Console.WriteLine ("\nInsert(1, \"Venus 2nd planet\")");
             Console.WriteLine ("\nInsert(2, \"Earth 3rd planet\")");
             planetList4.Insert (1, "Venus");
-
-            // Insert a Earth at position 3.
-            // Console.WriteLine ("\nInsert(2, \"Earth\")");
             planetList4.Insert (2, "Earth");
-
-            //Console.WriteLine();
-            // foreach (string planet in planetList4) {
-            //     Console.WriteLine (planet);
-            // }
 
             // 1 Mercury, 2 Venus, 3 Earth, 4  Mars, 5 Jupiter,  6 Saturn, 
             //  7 Uranus, 8 Neptune, 9 Pluto
@@ -89,6 +81,50 @@ namespace lists {
             planetList4.Add ("Pluto");
 
             foreach (string planet in planetList4) {
+                Console.WriteLine (planet);
+            }
+
+            // 1 Mercury, 2 Venus, 3 Earth, 4  Mars, 5 Jupiter,  6 Saturn, 
+            //  7 Uranus, 8 Neptune, 9 Pluto
+            // The four rocky planets are Mercury index 0, Venus index 1, Earth index 2 and Mars index 3.
+
+            Console.WriteLine ("\nrockyPlanets = planetList4.GetRange(0, 3).ToArray()");
+            string[] rockyPlanets = planetList4.GetRange (0, 4).ToArray ();
+
+            Console.WriteLine ();
+            foreach (string planet in rockyPlanets) {
+                Console.WriteLine (planet);
+            }
+
+            List<string> planetList5 = new List<string> () { "Mercury", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
+            // Use Insert() to add Earth 3, and Venus 2 in the correct order.
+
+            // Insert a Venus at position 2.
+
+            Console.WriteLine ("\nInsert(1, \"Venus 2nd planet\")");
+            Console.WriteLine ("\nInsert(2, \"Earth 3rd planet\")");
+            planetList5.Insert (1, "Venus");
+            planetList5.Insert (2, "Earth");
+
+            // 1 Mercury, 2 Venus, 3 Earth, 4  Mars, 5 Jupiter,  6 Saturn, 
+            //  7 Uranus, 8 Neptune, 9 Pluto
+
+            // Use Add() again to add Pluto to the end of the list.
+
+            planetList5.Add ("Pluto");
+
+            foreach (string planet in planetList5) {
+                Console.WriteLine (planet);
+            }
+
+            //use the Remove() method to eliminate Pluto from the end of planetList.
+
+            Console.WriteLine ("\nRemoveAt(8)");
+            // This will remove the part at index 3.
+            planetList5.RemoveAt (8);
+
+            Console.WriteLine ();
+            foreach (string planet in planetList5) {
                 Console.WriteLine (planet);
             }
 
