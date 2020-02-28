0f8d8cfb2f37f8b3df14eb86fbc37d75657dd510
ETamez9173 <elitamez17@gmail.com>
2020-02-28 15:45:50 -0600

    completed random numbers

---
+++ exercises/dictionaries/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionaries {
+    class Program {
+        static void Main (string[] args) {
+
+            List<string> planetList = new List<string> () { "Mercury", "Mars" };
+
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
--- exercises/lists/Program.cs
+++ exercises/lists/Program.cs
@@ -128,6 +128,40 @@ namespace lists {
                 Console.WriteLine (planet);
             }
 
+            //create a list of random numbers. Each number will be between 0 and 5.
+
+            Random random = new Random ();
+            List<int> numbers = new List<int> {
+                // random.Next (10),
+                // random.Next (10),
+                // random.Next (10),
+                // random.Next (10),
+                // random.Next (10),
+                // random.Next (10)
+
+                random.Next (6),
+                random.Next (6),
+                random.Next (6),
+                random.Next (6),
+                random.Next (6),
+                random.Next (6)
+            };
+
+            for (int i = 0; i < numbers.Count; i++) {
+                // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+
+                if (numbers[i] > 0 && numbers[i] < 6) {
+
+                    Console.WriteLine ($"numbers list contains {numbers[i]}");
+                } else {
+
+                    // if i != numbers[i] 
+
+                    Console.WriteLine ($"numbers list does not contain {numbers[i]}");
+                }
+
+            }
+
         }
     }
 }
